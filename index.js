a0376c0e624f41707b970d16f045c65a2bab3af0
Zoe Harmaning <zoeharmaning@u.boisestate.edu>
2026-09-22 17:32:02 -0600

    Jacks fault

--- index.js
+++ index.js
@@ -111,6 +111,6 @@ app.post("/api/sensor", (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(8080, () => {
+    console.log("Server running on port 8080");
 });
